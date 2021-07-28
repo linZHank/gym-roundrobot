7badadeee36d687aa52ac37cd044944fe75e8291
linzhank <linzhank@gmail.com>
2021-07-28 13:04:11 -0500

    create logger.urdf

---
+++ test.py
@@ -0,0 +1,18 @@
+import pybullet as p
+import time
+import pybullet_data
+physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+p.setGravity(0,0,-10)
+planeId = p.loadURDF("plane.urdf")
+startPos = [0,0,1]
+startOrientation = p.getQuaternionFromEuler([0,0,0])
+botId = p.loadURDF("resources/urdf/logger.urdf",startPos, startOrientation)
+#set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
+for i in range (300):
+    p.stepSimulation()
+    time.sleep(1./30.)
+botPos, botOrn = p.getBasePositionAndOrientation(botId)
+print(botPos,botOrn)
+p.disconnect()
+
