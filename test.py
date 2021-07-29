9babc0173f3df19d07ffa6741c965386cbf0c56f
linzhank <linzhank@gmail.com>
2021-07-29 11:33:55 -0500

    add caster model

--- test.py
+++ test.py
@@ -4,15 +4,15 @@ import pybullet_data
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 p.setGravity(0,0,0)
-planeId = p.loadURDF("plane.urdf")
-startPos = [0,0,1]
+# planeId = p.loadURDF("plane.urdf")
+startPos = [0,0,0]
 startOrientation = p.getQuaternionFromEuler([0,0,0])
-botId = p.loadURDF("urdf/micro_motor.urdf",startPos, startOrientation)
+botId = p.loadURDF("urdf/roundrobot.urdf",startPos, startOrientation)
 #set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
-for i in range (3000):
-    p.stepSimulation()
-    time.sleep(1./30.)
-botPos, botOrn = p.getBasePositionAndOrientation(botId)
-print(botPos,botOrn)
-p.disconnect()
+# for i in range (3000):
+#     p.stepSimulation()
+#     time.sleep(1./30.)
+# botPos, botOrn = p.getBasePositionAndOrientation(botId)
+# print(botPos,botOrn)
+# p.disconnect()
 
