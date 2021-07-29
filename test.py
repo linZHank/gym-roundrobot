ec052d1dd463927d6aa32d5d905d774457daa104
linzhank <linzhank@gmail.com>
2021-07-28 20:04:14 -0500

    convert .stp to .dae

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import time
 import pybullet_data
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
-p.setGravity(0,0,-10)
+p.setGravity(0,0,0)
 planeId = p.loadURDF("plane.urdf")
 startPos = [0,0,1]
 startOrientation = p.getQuaternionFromEuler([0,0,0])
-botId = p.loadURDF("resources/urdf/logger.urdf",startPos, startOrientation)
+botId = p.loadURDF("urdf/micro_motor.urdf",startPos, startOrientation)
 #set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
-for i in range (300):
+for i in range (3000):
     p.stepSimulation()
     time.sleep(1./30.)
 botPos, botOrn = p.getBasePositionAndOrientation(botId)
