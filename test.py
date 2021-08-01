240ffd18be0a25633fedfb7c5d1bab1b8e9a6031
linzhank <linzhank@gmail.com>
2021-08-01 13:14:23 -0500

    create round robot sdf

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 p.setGravity(0,0,0)
 # planeId = p.loadURDF("plane.urdf")
-startPos = [0,0,0]
-startOrientation = p.getQuaternionFromEuler([0,0,0])
-botId = p.loadURDF("urdf/roundrobot.urdf",startPos, startOrientation)
+# startPos = [0,0,0]
+# startOrientation = p.getQuaternionFromEuler([0,0,0])
+botId = p.loadSDF("model_data/roundrobot.sdf")
 #set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
 # for i in range (3000):
 #     p.stepSimulation()
